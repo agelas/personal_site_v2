c204c011bb6c337944a7d5fa967574f2d77b7cee
Mathias <mathiaswi177@gmail.com>
2023-03-05 15:29:48 -0800

    Motion transitions

--- src/components/Services.js
+++ src/components/Services.js
@@ -32,15 +32,27 @@ const Services = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
           {/* text */}
-          <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"> 
+          <motion.div 
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
+          > 
             <h2 className="h2 text-accent mb-6">What I Do.</h2>
             <h3 className="h3 max-w-[455px] mb-16">
               I'm a Freelance Front-end Developer with over 5 years of experience.
             </h3>
             <button className="btn btn-sm">See my work</button>
-          </div>
+          </motion.div>
           {/* services */}
-          <div className="flex-1">
+          <motion.div 
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
             {/* Services list */ }
             <div>
               {services.map((service, index) => {
@@ -59,7 +71,7 @@ const Services = () => {
                 );
               })}
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
