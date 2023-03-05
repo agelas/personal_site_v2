bf44e5275001a26067e8c10c02bea71278859a6c
Mathias <mathiaswi177@gmail.com>
2023-03-04 20:38:08 -0800

    Anchors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className='flex justify-between items-center'>
+          {/*  Logo that needs to change */}
+          <a href="#">
+            <img src={Logo} alt="" />
+          </a>
+          {/* Button */}
+          <button className="btn btn-sm">
+            Work with Me
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
