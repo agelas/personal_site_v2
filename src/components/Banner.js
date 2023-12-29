1182891a9aa1451786af83da251ef4ac24b78ba2
Mathias <mathiaswi177@gmail.com>
2023-12-28 22:37:36 -0800

    Update and simplify banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,24 +13,24 @@ const Banner = () => {
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* Text, make lg:text-[95px] for your name if you want one-line */}
           <div className="flex-1 text-center font-secondary lg:text-left">
-            <motion.h1 
-              variants={fadeIn("up", 0.3)} 
-              initial="hidden" 
-              whileInView={"show"} 
-              viewport={{once: false, amount: 0.7}} 
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="text-[55px] font-bold leading-[0.8] lg:text-[95px]"
             >
               Mathias <span>Insley</span>
             </motion.h1>
-            <motion.div 
-              variants={fadeIn("up", 0.4)} 
-              initial="hidden" 
-              whileInView={"show"} 
-              viewport={{once: false, amount: 0.7}}
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="mb-6 text-[36px] lg:text-[60px] leading-[1]"
             >
               <span className="text-white mr-4">
-                I am 
+                I am
               </span>
               <TypeAnimation sequence={[
                 'a Developer',
@@ -39,38 +39,28 @@ const Banner = () => {
                 2000,
                 'a Designer',
                 2000,
-              ]} 
-              speed={50}
-              className="text-accent"
-              wrapper="span"
-              repeat={Infinity}
+              ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
               />
             </motion.div>
-            <motion.p 
-              variants={fadeIn("up", 0.5)} 
-              initial="hidden" 
-              whileInView={"show"} 
-              viewport={{once: false, amount: 0.7}}
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="mb-8 max-w-lg mx-auto lg:mx-0"
             >
-              Welcome to my website!  
+              Welcome to my website!
             </motion.p>
-            <motion.div 
-              variants={fadeIn("up", 0.6)} 
-              initial="hidden" 
-              whileInView={"show"} 
-              viewport={{once: false, amount: 0.7}}
-              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
-            >
-              <button className="btn btn-lg">Contact Me</button>
-              <a href="#" className="text-gradient btn-link">My Portfolio</a>
-            </motion.div>
             {/* Socials */}
-            <motion.div 
-              variants={fadeIn("up", 0.7)} 
-              initial="hidden" 
-              whileInView={"show"} 
-              viewport={{once: false, amount: 0.7}}
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
             >
               <a href="https://github.com/agelas" target="_blank" rel="noreferrer">
@@ -82,10 +72,10 @@ const Banner = () => {
             </motion.div>
           </div>
           {/* Image */}
-          <motion.div 
-            variants={fadeIn("down", 0.5)} 
-            initial="hidden" 
-            whileInView={"show"} 
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
             className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mix-blend-lighten"
           >
             <img src={Image} alt="Headshot" className="rounded object-cover" />
